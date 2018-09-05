d634b91f0850d273c2bd4a00fdfde6ab73ee3350
limengyang <limengyang@139.com>
2018-09-05 14:29:35 +0800

    新分支master上的修改

--- src/main/java/com/limy/service/impl/TestServiceImpl.java
+++ src/main/java/com/limy/service/impl/TestServiceImpl.java
@@ -9,7 +9,7 @@ public class TestServiceImpl implements TestService{
 
     @Override
     public String sayHello(String str) {
-        System.out.println("这是一个新分支");
+        System.out.println("这是一个新分支master");
         return "hello 欢迎"+str;
     }
 }
